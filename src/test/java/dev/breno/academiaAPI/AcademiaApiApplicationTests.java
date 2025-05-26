9c6e456fcb4db79f1d6bf13ce8af607d126a3d8e
BrenoLuizC <blbrenomc@gmail.com>
2025-05-26 20:20:04 -0300

    first commit

---
+++ src/test/java/dev/breno/academiaAPI/AcademiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.breno.academiaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
