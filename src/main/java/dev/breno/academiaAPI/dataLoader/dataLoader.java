f0e127b9d1c31bc997bd40c9ca54ba2d0468b8c0
BrenoLuizC <blbrenomc@gmail.com>
2025-06-01 23:28:32 -0300

    Usando DataLoader para testar API

--- src/main/java/dev/breno/academiaAPI/dataLoader/dataLoader.java
+++ src/main/java/dev/breno/academiaAPI/dataLoader/dataLoader.java
@@ -1,4 +1,22 @@
 package dev.breno.academiaAPI.dataLoader;
 
-public class dataLoader {
+import dev.breno.academiaAPI.model.clientModel;
+import dev.breno.academiaAPI.repository.clientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class dataLoader implements CommandLineRunner {
+
+    @Autowired
+    private clientRepository ClientRepository;
+
+    @Override
+    public void run(String... args) {
+        ClientRepository.save(new clientModel(null, "test", 10));
+        ClientRepository.save(new clientModel(null, "testando", 100));
+
+        System.out.println("Dados inseridos com sucesso!");
+    }
 }
